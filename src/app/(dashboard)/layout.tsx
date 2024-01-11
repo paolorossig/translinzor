5d12e5390eb13a5e0c9c9202af4720b40d0901de
Paolo Rossi <mpaolo.rossig@gmail.com>
2024-01-11 02:15:05 -0500

    feat: add nav-breadcrumb to header

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
-import { ChevronRightIcon, HomeIcon } from 'lucide-react'
-
+import NavBreadcrumb from '@/components/layout/nav-breadcrumb'
 import Sidebar from '@/components/layout/sidebar'
 
 export default function DashboardLayout({
@@ -13,19 +12,12 @@ export default function DashboardLayout({
         <Sidebar />
       </div>
       <div className="h-full bg-card lg:ml-72 lg:mr-4 lg:rounded-t-2xl">
-        <header className="sticky top-0 z-40 flex h-14 shrink-0 items-center gap-x-4 bg-card px-4 sm:gap-x-6 sm:px-6 lg:rounded-t-2xl lg:px-8">
+        <header className="sticky top-0 z-40 flex h-12 shrink-0 items-center gap-x-4 bg-card px-4 sm:gap-x-6 sm:px-6 lg:rounded-t-2xl">
           <div className="flex flex-1 self-stretch">
-            <nav
-              className="flex items-center space-x-4 text-muted-foreground"
-              aria-label="breadcrumb"
-            >
-              <HomeIcon className="h-4 w-4 font-semibold" />
-              <ChevronRightIcon className="h-4 w-4 font-semibold" />
-              <span>Dashboard</span>
-            </nav>
+            <NavBreadcrumb />
           </div>
         </header>
-        <main className="bg-card py-4 text-card-foreground">
+        <main className="bg-card py-2 text-card-foreground">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             {children}
           </div>
