e794329be917831e88a313516973e45457686406
Paolo Rossi <mpaolo.rossig@gmail.com>
2024-01-19 10:22:14 -0500

    fix: layout border in dark mobile

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -13,13 +13,13 @@ export default function DashboardLayout({
         <Sidebar />
       </div>
       <div className="h-full bg-card lg:ml-72 lg:mr-4 lg:rounded-t-2xl">
-        <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 bg-card px-4 dark:border dark:border-b-transparent sm:gap-x-6 sm:px-6 lg:rounded-t-2xl">
+        <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 bg-card px-4 dark:border-b-transparent sm:gap-x-6 sm:px-6 lg:rounded-t-2xl dark:lg:border">
           <div className="flex flex-1 self-stretch">
             <NavBreadcrumb />
           </div>
           <ThemeToggle />
         </header>
-        <main className="bg-card py-2 text-card-foreground dark:border dark:border-y-transparent">
+        <main className="bg-card py-2 text-card-foreground dark:border-y-transparent dark:lg:border">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             {children}
           </div>
