6f6a9ad33d03e1ddb99ee95fd3e6d3dc72154f6e
Paolo Rossi <mpaolo.rossig@gmail.com>
2024-01-19 17:09:37 -0500

    fix: border style for dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -16,8 +16,8 @@ export default function DashboardLayout({
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         <Sidebar />
       </div>
-      <div className="h-full bg-card lg:ml-72 lg:mr-4 lg:rounded-t-2xl">
-        <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 bg-card px-4 dark:border-b-transparent sm:gap-x-6 sm:px-6 lg:rounded-t-2xl dark:lg:border">
+      <div className="h-full border-x bg-card lg:ml-72 lg:mr-4 lg:rounded-t-2xl lg:border-t">
+        <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 bg-card px-4 sm:gap-x-6 sm:px-6 lg:rounded-t-2xl">
           <Sheet>
             <SheetTrigger asChild>
               <Button size="icon" variant="outline" className="p-2.5 lg:hidden">
@@ -35,7 +35,7 @@ export default function DashboardLayout({
           </div>
           <ThemeToggle />
         </header>
-        <main className="bg-card py-2 text-card-foreground dark:border-y-transparent dark:lg:border">
+        <main className="bg-card py-2 text-card-foreground">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             {children}
           </div>
