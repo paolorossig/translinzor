5679d0a88c2bdfe721464056d3bdc98fc0831d0d
Paolo Rossi <mpaolo.rossig@gmail.com>
2024-01-19 03:14:30 -0500

    feat: add dropzone and xlsx files reader

--- src/components/shipment-bulk-upload.tsx
+++ src/components/shipment-bulk-upload.tsx
@@ -1,5 +1,10 @@
-import { UploadIcon } from 'lucide-react'
+'use client'
 
+import { useEffect, useState } from 'react'
+import { UploadIcon } from 'lucide-react'
+import * as xlsx from 'xlsx'
+
+import { Dropzone } from '@/components/dropzone'
 import { Button } from '@/components/ui/button'
 import {
   Drawer,
@@ -12,10 +17,37 @@ import {
   DrawerTrigger,
 } from '@/components/ui/drawer'
 
+const excelAcceptedMimeTypes = {
+  'application/vnd.ms-excel': ['.xls'],
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': [
+    '.xlsx',
+  ],
+}
+
 export function ShipmentBulkUpload() {
+  const [excelFile, setExcelFile] = useState<ArrayBuffer | null>(null)
+
+  const onDrop = (acceptedFiles: File[]) => {
+    const reader = new FileReader()
+    reader.readAsArrayBuffer(acceptedFiles[0])
+    reader.onload = (e) => setExcelFile(e.target?.result as ArrayBuffer | null)
+    reader.onabort = () => console.log('file reading was aborted')
+    reader.onerror = () => console.log('file reading has failed')
+  }
+
+  useEffect(() => {
+    if (!excelFile) return
+
+    const workbook = xlsx.read(excelFile, { type: 'buffer' })
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+    const data = xlsx.utils.sheet_to_json(worksheet)
+
+    console.log(data)
+  }, [excelFile])
+
   return (
     <Drawer>
-      <DrawerTrigger>
+      <DrawerTrigger asChild>
         <Button className="h-8">
           <UploadIcon className="mr-2 h-4 w-4" />
           <span>Carga masiva</span>
@@ -24,17 +56,20 @@ export function ShipmentBulkUpload() {
       <DrawerContent>
         <div className="mx-auto w-full max-w-xl">
           <DrawerHeader>
-            <DrawerTitle className="text-card-foreground">
-              Carga masiva
-            </DrawerTitle>
+            <DrawerTitle className="text-primary">Carga masiva</DrawerTitle>
             <DrawerDescription>
               Esta acción subirá las entregas programadas mediante un template
               de excel.
             </DrawerDescription>
           </DrawerHeader>
+          <Dropzone
+            maxFiles={1}
+            accept={excelAcceptedMimeTypes}
+            onDrop={onDrop}
+          />
           <DrawerFooter>
             <Button>Subir</Button>
-            <DrawerClose>
+            <DrawerClose asChild>
               <Button variant="outline" className="w-full">
                 Cancelar
               </Button>
