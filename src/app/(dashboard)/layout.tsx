96931d1a75cd448a85c905696412e62d2a4e7f18
Paolo Rossi <mpaolo.rossig@gmail.com>
2024-01-21 17:44:26 -0500

    refactor: dashboard layout for fixed main height

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,11 +12,11 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="h-full bg-background text-foreground lg:pt-4">
+    <div className="h-full bg-background text-foreground">
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         <Sidebar />
       </div>
-      <div className="h-full bg-card lg:ml-72 lg:mr-4 lg:rounded-t-2xl lg:border-x lg:border-t">
+      <div className="fixed inset-x-0 top-0 h-full bg-card lg:ml-72 lg:mr-4 lg:mt-4 lg:rounded-t-2xl lg:border-x lg:border-t">
         <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 bg-card px-4 sm:gap-x-6 sm:px-6 lg:rounded-t-2xl">
           <Sheet>
             <SheetTrigger asChild>
@@ -35,8 +35,8 @@ export default function DashboardLayout({
           </div>
           <ThemeToggle />
         </header>
-        <main className="h-full bg-card py-2 text-card-foreground">
-          <div className="mx-auto h-full max-w-7xl px-4 sm:px-6 lg:px-8">
+        <main className="h-[calc(100%-5rem)] bg-card py-2 text-card-foreground">
+          <div className="mx-auto h-full max-w-7xl overflow-y-auto px-4 sm:px-6 lg:px-8">
             {children}
           </div>
         </main>
