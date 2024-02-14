db2adcc8f0916cd9907df5c9b28086975482c89c
Paolo Rossi <mpaolo.rossig@gmail.com>
2024-02-13 22:52:26 -0500

    feat: add refused reason in a popover

--- src/app/(dashboard)/shipments/[shipmentId]/columns.tsx
+++ src/app/(dashboard)/shipments/[shipmentId]/columns.tsx
@@ -1,12 +1,20 @@
 'use client'
 
 import { type ColumnDef } from '@tanstack/react-table'
+import { InfoIcon } from 'lucide-react'
 
 import { Icons } from '@/components/icons'
 import {
   getOrderStatus,
+  OrderStatus,
   orderStatusOptions,
 } from '@/components/modules/shipments/order-status'
+import { Button } from '@/components/ui/button'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 import { type ShipmentById } from '@/lib/actions'
 
 export const columns: ColumnDef<ShipmentById['orders'][number]>[] = [
@@ -46,7 +54,22 @@ export const columns: ColumnDef<ShipmentById['orders'][number]>[] = [
       return (
         <div className="flex items-center">
           {Icon && <Icon className="mr-2 h-4 w-4 text-muted-foreground" />}
-          <span>{status.label}</span>
+          <p>{status.label}</p>
+          {status.value === OrderStatus.REFUSED && (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button size="icon" variant="outline" className="ml-2">
+                  <InfoIcon className="h-4 w-4" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent align="end" className="space-y-2">
+                <h4 className="text-sm font-medium leading-none">Razón</h4>
+                <p className="text-sm text-muted-foreground">
+                  {row.original.refusedReason}
+                </p>
+              </PopoverContent>
+            </Popover>
+          )}
         </div>
       )
     },
