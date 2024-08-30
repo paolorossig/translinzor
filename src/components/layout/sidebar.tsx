7e6e6ffd172873787068c18f16b035b07f8f4c65
Paolo Rossi <mpaolo.rossig@gmail.com>
2024-08-29 23:17:56 -0500

    chore: fix alignment on sidebar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -37,7 +37,7 @@ export default function Sidebar({ displayName, userNavigation }: SidebarProps) {
   }
 
   return (
-    <div className="flex h-full grow flex-col gap-y-5 overflow-y-auto bg-background px-6 pb-4">
+    <div className="flex h-full grow flex-col gap-y-2 overflow-y-auto bg-background px-6 pb-4">
       <Link href="/" className="flex h-16 shrink-0 items-center">
         <img
           className="h-16 w-auto"
