2abde75a6e530f109c896d8b225072f963bd0019
Paolo Rossi <mpaolo.rossig@gmail.com>
2024-09-05 19:33:29 -0500

    feat: add effectiveness progress and top costumers to dashboard page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,60 +1,88 @@
+import { addMonths, endOfMonth, isSameMonth, startOfMonth } from 'date-fns'
 import { CalendarIcon, ListOrderedIcon, TruckIcon } from 'lucide-react'
 
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { getMetrics } from '@/db/queries'
+import { MetricCard } from '@/components/modules/metrics/card'
+import { EffectivenessProgress } from '@/components/modules/metrics/effectiveness-progress'
+import { TopCostumers } from '@/components/modules/metrics/top-costumers'
+import {
+  getCostumerMetrics,
+  getShipmentMetrics,
+  getTotalsMetrics,
+} from '@/db/queries'
+import { formatDateRange } from '@/lib/utils'
 
 export default async function Home() {
-  const metrics = await getMetrics()
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+  const from = startOfMonth(addMonths(today, -3))
+  const to = endOfMonth(today)
+  const l3MDateRange = formatDateRange(from, to)
+
+  const [totals, topCostumers, l3mMetrics] = await Promise.all([
+    getTotalsMetrics(),
+    getCostumerMetrics({ limit: 8 }),
+    getShipmentMetrics({ aggregator: 'deliveryDate', from, to }),
+  ])
+
+  const l3mTotals = l3mMetrics.reduce(
+    (acc, curr) => {
+      acc.count += curr.orders.count
+      acc.delivered += curr.orders.delivered
+      return acc
+    },
+    { count: 0, delivered: 0 },
+  )
+
+  const l1mTotals = l3mMetrics.reduce(
+    (acc, curr) => {
+      if (!isSameMonth(new Date(curr.aggregator), today)) return acc
+      acc.count += curr.orders.count
+      acc.delivered += curr.orders.delivered
+      return acc
+    },
+    { count: 0, delivered: 0 },
+  )
+
+  const l3mEffectiveness = Math.round(
+    (l3mTotals.delivered / l3mTotals.count) * 100,
+  )
+  const l1mEffectiveness = l1mTotals.count
+    ? Math.round((l1mTotals.delivered / l1mTotals.count) * 100)
+    : 100
 
   return (
     <>
       <h1 className="scroll-m-20 pb-2 text-2xl font-medium tracking-tight text-primary">
         Dashboard
       </h1>
-      <div className="grid gap-x-6 gap-y-3 py-4 sm:grid-cols-3">
+      <div className="grid gap-3 py-4 sm:grid-cols-3 sm:gap-6">
         <MetricCard
           title="Entregas totales"
-          value={metrics.totalShipments}
+          value={totals.shipments}
           detail="Esto es el número total de entregas"
           icon={TruckIcon}
         />
         <MetricCard
           title="Ordenes totales"
-          value={metrics.totalOrders}
+          value={totals.orders}
           detail="Esto es el número total de ordenes"
           icon={ListOrderedIcon}
         />
         <MetricCard
           title="Días con entregas"
-          value={metrics.totalDaysWithShipments}
+          value={totals.daysWithShipments}
           detail="Esto es el número total de días con entregas"
           icon={CalendarIcon}
         />
+        <EffectivenessProgress
+          description={l3MDateRange}
+          l3mValue={l3mEffectiveness}
+          l1mValue={l1mEffectiveness}
+        />
+        <div className="sm:col-span-2 sm:col-start-2">
+          <TopCostumers description={l3MDateRange} data={topCostumers} />
+        </div>
       </div>
     </>
-  )
-}
-
-interface MetricCardProps {
-  title: string
-  value: number
-  detail: string
-  icon: React.ComponentType<{ className?: string }>
-}
-
-function MetricCard({ title, value, detail, icon: CardIcon }: MetricCardProps) {
-  return (
-    <Card>
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">{title}</CardTitle>
-        <CardIcon className="h-4 w-4" />
-      </CardHeader>
-      <CardContent>
-        <div className="text-2xl font-bold text-primary">
-          {new Intl.NumberFormat().format(value)}
-        </div>
-        <p className="text-xs text-gray-500">{detail}</p>
-      </CardContent>
-    </Card>
   )
 }
